58b0e6d051fd27eac3186dc2d88612666df3a61b
furu0202 <furuka.0202@gmail.com>
2023-06-10 01:05:11 +0900

    ヘッダーの追加

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
-// import styles from './Footer.module.css';
+import styles from './Header.module.css';
 
 export function Header() {
   return (
-    <header>
-      <Link href='/'>Index</Link>
+    <header className={styles.header}>
+      <Link href='/' className={styles.anchor}>
+        Index
+      </Link>
       <Link href='/about'>About</Link>
     </header>
   );
