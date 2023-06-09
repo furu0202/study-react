20aedbe21c0a8cb4122e9c4f514d002659368f76
furu0202 <furuka.0202@gmail.com>
2023-06-10 01:16:21 +0900

    アイテムを各配列に格納

--- components/Links.js
+++ components/Links.js
@@ -1,5 +1,29 @@
 import styles from './Links.module.css';
 
+const ITEMS = [
+  {
+    href: 'https://nextjs.org/docs',
+    title: 'Documentation &rarr',
+    description: 'Find in-depth information about Next.js features and API.',
+  },
+  {
+    href: 'https://nextjs.org/learn',
+    title: 'Learn &rarr',
+    description: 'Learn about Next.js in an interactive course with quizzes!.',
+  },
+  {
+    href: 'https://github.com/vercel/next.js/tree/master/examples',
+    title: 'Examples &rarr',
+    description: 'Discover and deploy boilerplate example Next.js projects.',
+  },
+  {
+    href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+    title: 'Deploy &rarr',
+    description:
+      'Instantly deploy your Next.js site to a public URL with Vercel.',
+  },
+];
+
 export function Links() {
   return (
     <div className={styles.grid}>
