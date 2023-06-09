6551a9799967a1b239c39596269e85e7079b5987
furu0202 <furuka.0202@gmail.com>
2023-06-10 00:35:22 +0900

    Header コンポーネント作成

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import styles from './Footer.module.css';
+
+export function Header() {
+  return (
+    <header>
+      <a href='/'>Index </a>
+      <a href='/'>About </a>
+    </header>
+  );
+}
