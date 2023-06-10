f39ff688b01ece2976652aa00a6408509e50ae5c
furu0202 <furuka.0202@gmail.com>
2023-06-11 07:10:41 +0900

    Unicode文字列の実践

--- components/Links.js
+++ components/Links.js
@@ -4,22 +4,22 @@ import styles from './Links.module.css';
 const ITEMS = [
   {
     href: 'https://nextjs.org/docs',
-    title: 'Documentation &rarr',
+    title: 'Documentation \u2192',
     description: 'Find in-depth information about Next.js features and API.',
   },
   {
     href: 'https://nextjs.org/learn',
-    title: 'Learn &rarr',
+    title: 'Learn \u2192',
     description: 'Learn about Next.js in an interactive course with quizzes!.',
   },
   {
     href: 'https://github.com/vercel/next.js/tree/master/examples',
-    title: 'Examples &rarr',
+    title: 'Examples \u2192',
     description: 'Discover and deploy boilerplate example Next.js projects.',
   },
   {
     href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
-    title: 'Deploy &rarr',
+    title: 'Deploy \u2192',
     description:
       'Instantly deploy your Next.js site to a public URL with Vercel.',
   },
