a83d291f03ea03db509608e9c4a401ead843eb37
furu0202 <furuka.0202@gmail.com>
2023-06-13 00:43:37 +0900

    useCallbackのテスト実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,21 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from 'src/components/Footer';
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
+import { useCallback } from 'react';
+
+// const handleClick = (e, foo) => {
+//   console.log(e.target.href);
+//   e.preventDefault();
+//   alert(foo);
+// };
 
 export default function Home() {
-  const handleClick = (e) => {
+  const foo = 1;
+  const handleClick = useCallback((e) => {
     console.log(e.target.href);
     e.preventDefault();
-    // alert(123);
-  };
+    alert(foo);
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
