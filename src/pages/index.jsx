294cb5b4dff64faaf5c8f144f51536e4b60ad250
furu0202 <furuka.0202@gmail.com>
2023-06-17 18:12:27 +0900

    useCallvackを用いてカウントアップボタンを作成

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from 'src/components/Footer';
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 // const handleClick = (e, foo) => {
 //   console.log(e.target.href);
@@ -13,21 +13,22 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [count, setCount] = useState(1);
-  // let foo = 1;
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    // foo = foo + 1;
-  };
+  const handleClick = useCallback(
+    (e) => {
+      console.log(count);
+      if (count < 10) {
+        setCount((count) => count + 1);
+      }
+    },
+    [count]
+  );
 
   useEffect(() => {
-    console.log('foo');
     document.body.style.backgroundColor = 'lightblue';
     return () => {
-      console.log('bar');
       document.body.style.backgroundColor = 'pink';
     };
-  }, [count]);
+  }, []);
 
   return (
     <div className={styles.container}>
