dd7ebdd321c8f97403d7d3fa609c7c22e31b3783
furu0202 <furuka.0202@gmail.com>
2023-06-24 18:13:49 +0900

    items,handleReduceのリフトアップ

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -1,42 +1,11 @@
 import Link from 'next/link';
-import { useCallback, useState } from 'react';
 import styles from 'src/components/Links/Links.module.css';
 
-const ITEMS = [
-  {
-    href: 'https://nextjs.org/docs',
-    title: 'Documentation \u2192',
-    description: 'Find in-depth information about Next.js features and API.',
-  },
-  {
-    href: 'https://nextjs.org/learn',
-    title: 'Learn \u2192',
-    description: 'Learn about Next.js in an interactive course with quizzes!.',
-  },
-  {
-    href: 'https://github.com/vercel/next.js/tree/master/examples',
-    title: 'Examples \u2192',
-    description: 'Discover and deploy boilerplate example Next.js projects.',
-  },
-  {
-    href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
-    title: 'Deploy \u2192',
-    description:
-      'Instantly deploy your Next.js site to a public URL with Vercel.',
-  },
-];
-
-export function Links() {
-  const [items, setItems] = useState([ITEMS]);
-  const handleReduce = useCallback(() => {
-    setItems((prevItems) => {
-      return prevItems.slice(0, prevItems.length - 1);
-    });
-  }, []);
+export function Links({ items, handleReduce }) {
   return (
     <div className={styles.grid}>
       <button onClick={handleReduce}>減らす</button>
-      {ITEMS.map((item) => {
+      {items.map((item) => {
         return (
           <Link href={item.href} className={styles.card} key={item.href}>
             <h2 className={styles.title}>■{item.title}</h2>
